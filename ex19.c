ff8baf89de7fd48e3cf0bb7536155cafdaad2c47
veraghazaryan <I24.Ghazaryan.Vera@etud.ufar.am>
2025-09-25 06:53:28 +0400

    first commit

---
+++ ex19.c
@@ -0,0 +1,58 @@
+/** Ex 1.19 **/
+
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int _getline(char line[], int lim);
+
+void reverse(char rline[]);
+
+int main(void) {
+    int len;
+    char line[MAXLINE];
+
+    while ((len = _getline(line, MAXLINE)) > 0) {
+        reverse(line);
+        printf("%s", line);
+    }
+
+    return 0;
+}
+
+int _getline(char line[], int lim) {
+    int i, c;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+
+    if (c == '\n') {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+
+    return i;
+}
+
+void reverse(char rline[]) {
+    int i, j;
+    char temp;
+
+    for (i = 0; rline[i] != '\0'; ++i);
+
+    --i;
+
+    if (rline[i] == '\n')
+        --i;
+
+    j = 0;
+
+    while (j < i) {
+        temp = rline[j];
+        rline[j] = rline[i];
+        rline[i] = temp;
+        --i;
+        ++j;
+    }
+}
